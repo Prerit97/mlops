a0b331f26fc4ca935479dc758fcb102685a8f031
Prerit97 <91933172+Prerit97@users.noreply.github.com>
2022-07-08 10:50:35 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -229,11 +229,3 @@ def pipeline(
 compiler.Compiler().compile(pipeline_func=pipeline,
         package_path='ml_winequality.json')        
     
-start_pipeline = pipeline_jobs.PipelineJob(
-    display_name="winequality-pipeline",
-    template_path="gs://mlops-bucket-wineqaulity/ml_winequality.json",
-    enable_caching=True,
-    location="europe-west1",
-)
-
-start_pipeline.run()
